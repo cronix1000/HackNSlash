c6285bc2accca6eeac758b0350034ba0dae3e700
Idan Gomberg <crackenyogs10@gmail.com>
2022-10-28 22:22:10 -0400

    Added enemies spawning in 4 cardinal directions

--- Assets/Scripts/EnemyGeneration.cs
+++ Assets/Scripts/EnemyGeneration.cs
@@ -6,9 +6,11 @@ public class EnemyGeneration : MonoBehaviour
 {
     public GameObject enemy;
     int mobCount;
-    Transform player;
+    public Transform player;
     Camera camera;
-
+    List<GameObject> monsterCount = new List<GameObject>();
+    int monsterIntCount = 0;
+    int mobMax = 0;
     private void Start()
     {
         camera = Camera.main;
@@ -16,32 +18,78 @@ public class EnemyGeneration : MonoBehaviour
 
     private void Update()
     {
-        SpawnGeneration(0);
+        CallDirections(0);
     }
 
-    void SpawnGeneration(int level) { 
-        List<GameObject> monsterCount = new List<GameObject>();
-        int mobMax = 0;
+    private void CallDirections(int level)
+    {
         switch (level)
         {
             case 0:
-                mobMax = 10;
+                mobMax = 40;
                 break;
             case 1:
-                mobMax = 20;
+                mobMax = 80;
                 break;
             case 2:
-                mobMax = 30;
+                mobMax = 120;
                 break;
             default:
                 break;
         }
-        while (monsterCount.Count < mobMax) {
+        while (monsterIntCount < mobMax)
+        {
+            SpawnGeneration(Direction.Up);
+            SpawnGeneration(Direction.Down);
+            SpawnGeneration(Direction.Left);
+            SpawnGeneration(Direction.Right);
+        }
+    }
+
+    void SpawnGeneration(Direction dir) {
+
+       
+
+            Debug.Log(mobMax);
             for (int i = 0; i < mobMax; i++)
             {
-                GameObject monster = Instantiate(enemy, new Vector3(camera.orthographicSize + 5, camera.orthographicSize + 5, 0f), Quaternion.identity) as GameObject;
+                float posY = 0;
+                float posX = 0;
+            
+                switch (dir)
+                {
+                    case Direction.Up:
+                    posY = player.transform.position.y + Random.Range(0, 10) + 8;
+                    posX = Random.Range(player.transform.position.x - 8, player.transform.position.x + 8);
+
+                        break;
+                    case Direction.Down:
+                    posY = player.transform.position.y - 8 + Random.Range(-10 , 0) - 8;
+                    posX = Random.Range(player.transform.position.x - 8, player.transform.position.x + 8);
+                        break;
+                    case Direction.Left:
+                        posX = player.transform.position.x - 8 + Random.Range(-10, 0) - 8 ;
+                        posY = Random.Range(player.transform.position.y - 8, player.transform.position.y + 8);
+                        break;
+                    case Direction.Right:
+                        posX = player.transform.position.x + 8 +  Random.Range(0, 10) + 8;
+                        posY = Random.Range(player.transform.position.y - 8, player.transform.position.y + 8);
+                        break;
+                }
+
+
+                GameObject monster = Instantiate(enemy, new Vector3(posX, 1, posY), Quaternion.identity) as GameObject;
                 monsterCount.Add(monster);
-            }
+                monsterIntCount += 1;
+            
+            
         }
     }
 }
+
+public enum Direction { 
+    Up,
+    Down,
+    Left,
+    Right
+} 
